bfe5dbd5c632582de7a119b84649d8fcac96ee98
Xavier Lefèvre <xavier.lefevre@me.com>
2017-05-07 21:36:03 +0200

    get first mower position

--- src/mower.js
+++ src/mower.js
@@ -1,6 +1,26 @@
+const retrieveLawn = input => input[0].split(' ').map(coordinate => parseInt(coordinate, 10));
+
+const getFirstMowerPosition = input => {
+  const mowerPosition = {};
+  input.split(' ').map((value, index) => {
+    if (index === 0) {
+      mowerPosition.x = parseInt(value, 10);
+    } else if (index === 1) {
+      mowerPosition.y = parseInt(value, 10);
+    } else {
+      mowerPosition.o = value;
+    }
+  });
+  return mowerPosition;
+};
+
 class Mower {
   constructor(input) {
-    this.lawn = input.split(' ').map(coordinate => parseInt(coordinate, 10));
+    this.mowers = [];
+    const inputLines = input.split('\n');
+
+    this.lawn = retrieveLawn(inputLines);
+    this.mowers.push(getFirstMowerPosition(inputLines[1]));
   }
 }
 
--- src/mower.test.js
+++ src/mower.test.js
@@ -1,8 +1,14 @@
 const Mower = require('./mower');
 
 describe('mower logic', () => {
+  const input = `5 5
+1 2 N`;
+
   it('generates the lawn', () => {
-    const input = '5 5';
     expect(new Mower(input).lawn).toEqual([5, 5]);
+  });
+
+  it('places the first mower', () => {
+    expect(new Mower(input).mowers).toEqual([{ x: 1, y: 2, o: 'N' }]);
   });
 });
